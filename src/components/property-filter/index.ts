1abb76df0b2d5cd8a7e15558880acdf4eb8bd7c9
Anas Abu dayah <anas.abudayah@gmail.com>
2024-12-27 20:01:14 -0800

    feat(new): property filter component

---
+++ src/components/property-filter/index.ts
@@ -0,0 +1,1 @@
+export { default as CPropertyFilter } from './property-filter';
---
+++ src/components/property-filter/property-filter.tsx
@@ -0,0 +1,42 @@
+import React, { useCallback } from 'react';
+import { Control, Controller, FieldValues, FieldPathValue, FieldPath, RegisterOptions } from 'react-hook-form';
+import { PropertyFilterProps, NonCancelableCustomEvent, PropertyFilter } from '@cloudscape-design/components';
+
+export interface ControlledPropertyFilterProps<T extends FieldValues>
+  extends Omit<PropertyFilterProps, 'query' | 'onChange'> {
+  name: FieldPath<T>;
+  control?: Control<T>;
+  defaultValue?: FieldPathValue<T, FieldPath<T>>;
+  rules?: Omit<RegisterOptions<T, FieldPath<T>>, 'valueAsNumber' | 'valueAsDate' | 'setValueAs' | 'disabled'>;
+  shouldUnregister?: boolean;
+}
+
+export const CPropertyFilter = <TFieldValues extends FieldValues>(
+  CPropertyFilterProperties: ControlledPropertyFilterProps<TFieldValues>
+) => {
+  const { name, control, defaultValue, rules, shouldUnregister = false, ...props } = CPropertyFilterProperties;
+  const handleOnChange = useCallback(
+    (
+      formOnChange: (query: PropertyFilterProps.Query) => void,
+      e: NonCancelableCustomEvent<PropertyFilterProps.Query>
+    ) => {
+      formOnChange(e.detail);
+    },
+    []
+  );
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      defaultValue={defaultValue}
+      rules={rules}
+      shouldUnregister={shouldUnregister}
+      render={({ field: { onChange, value } }) => {
+        return <PropertyFilter onChange={handleOnChange.bind(null, onChange)} query={value} {...props} />;
+      }}
+    />
+  );
+};
+
+export default CPropertyFilter;
