67a7686bb5296e64790121ec4ebcc1cf16641118
Anas Abu dayah <anas.abudayah@gmail.com>
2025-01-22 13:26:05 -0800

    chore: update demos assets path

--- demos-src/vite.config.ts
+++ demos-src/vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: '/react-hook-form-cloudscape/',
     build: {
-    outDir: '../docs',
-    emptyOutDir: true, // also necessary
-  }
+      assetsDir: '.',
+      outDir: '../docs',
+      emptyOutDir: true, // also necessary
+    }
 })
